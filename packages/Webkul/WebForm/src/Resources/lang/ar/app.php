13e9b26a530f6e76ea0cd43fa9bf9e24e4bc26ba
patile2024 <lengocphan050301@gmail.com>
2024-10-29 11:38:07 +0700

    feat: setup project base

---
+++ packages/Webkul/WebForm/src/Resources/lang/ar/app.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'acl' => [
+        'title'  => 'نماذج الويب',
+        'view'   => 'عرض',
+        'create' => 'إنشاء',
+        'edit'   => 'تحرير',
+        'delete' => 'حذف',
+    ],
+
+    'menu' => [
+        'title'      => 'نماذج الويب',
+        'title-info' => 'إضافة، تحرير أو حذف نماذج الويب من CRM',
+    ],
+
+    'validations' => [
+        'invalid-phone-number' => 'رقم الهاتف غير صالح',
+    ],
+];
---
+++ packages/Webkul/WebForm/src/Resources/lang/tr/app.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'acl' => [
+        'title'  => 'Web Formları',
+        'view'   => 'Görüntüle',
+        'create' => 'Oluştur',
+        'edit'   => 'Düzenle',
+        'delete' => 'Sil',
+    ],
+
+    'menu' => [
+        'title'      => 'Web Formları',
+        'title-info' => 'CRM’den web formları ekleyin, düzenleyin veya silin',
+    ],
+
+    'validations' => [
+        'invalid-phone-number' => 'Geçersiz telefon numarası',
+    ],
+];
